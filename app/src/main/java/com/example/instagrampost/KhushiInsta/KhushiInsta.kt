ec7981f199263e415a2038f51cbb907af28a0b67
I58gen <yashpal14700@gmail.com>
2024-02-09 15:36:04 +0530

    Add Packages

---
+++ app/src/main/java/com/example/instagrampost/KhushiInsta/KhushiInsta.kt
@@ -0,0 +1,12 @@
+package com.example.instagrampost.KhushiInsta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagrampost.R
+
+class KhushiInsta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_khushi_insta)
+    }
+}
---
+++ app/src/main/java/com/example/instagrampost/ShakshiInsta/ShakshiInsta.kt
@@ -0,0 +1,12 @@
+package com.example.instagrampost.ShakshiInsta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagrampost.R
+
+class ShakshiInsta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shakshi_insta)
+    }
+}
