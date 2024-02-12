19a1190a7c82ca4064868984aca94a2c3766e847
admin <kunalwarrior25@gmail.com>
2024-02-12 16:18:43 +0530

    kunal create kunal_instagram_page.xml

---
+++ app/src/main/java/com/example/instagrampost/KunalAdhiraInsta/kunal_instagram_modleclass.kt
@@ -0,0 +1,32 @@
+package com.example.instagrampost.KunalAdhiraInsta
+
+class kunal_instagram_modleclass {
+    var kunal_userimage = 0
+    var kunal_username = ""
+    var kunal_songname = ""
+    var kunal_postimage = 0
+    var kunal_likes_count = ""
+    var kunal_comment = ""
+    var kunal_comment_count = ""
+    var kunal_days_count = ""
+
+    constructor(
+        kunal_userimage: Int,
+        kunal_username: String,
+        kunal_songname: String,
+        kunal_postimage: Int,
+        kunal_likes_count: String,
+        kunal_comment: String,
+        kunal_comment_count: String,
+        kunal_days_count: String
+    ) {
+        this.kunal_userimage = kunal_userimage
+        this.kunal_username = kunal_username
+        this.kunal_songname = kunal_songname
+        this.kunal_postimage = kunal_postimage
+        this.kunal_likes_count = kunal_likes_count
+        this.kunal_comment = kunal_comment
+        this.kunal_comment_count = kunal_comment_count
+        this.kunal_days_count = kunal_days_count
+    }
+}
--- app/src/main/java/com/example/instagrampost/MainActivity.kt
+++ app/src/main/java/com/example/instagrampost/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.instagrampost
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.instagrampost.KunalAdhiraInsta.kunal_instagram_page
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
