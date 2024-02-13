123f1395f5ed67088718da4488df4015c33bdadf
lappy <shakshik732@gmail.com>
2024-02-13 16:09:25 +0530

    i add images in drawable

---
+++ app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_Adapterclass.kt
@@ -0,0 +1,8 @@
+package com.example.instagrampost.ShakshiInsta
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+class s_ins_Adapterclass() {
+
+}
---
+++ app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_modelclass.kt
@@ -0,0 +1,16 @@
+package com.example.instagrampost.ShakshiInsta
+
+class s_ins_modelclass {
+    var i_imag:Int=0
+    var i_comment:String=""
+    var i_share:Int=0
+    var i_save:Int=0
+    var i_like:String=""
+    constructor(i_imag:Int,i_comment:String,i_share:Int,i_save:Int,i_like:String){
+        this.i_imag=i_imag
+        this.i_comment=i_comment
+        this.i_share=i_share
+        this.i_save=i_save
+        this.i_like=i_like
+    }
+}
