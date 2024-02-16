90a989e3f811cd75dc04977c7abbcfdeaf84fc31
I58gen <yashpal14700@gmail.com>
2024-02-16 16:08:42 +0530

    updatepro 2

--- app/src/main/java/com/example/instagrampost/Yash/MainActivity.kt
+++ app/src/main/java/com/example/instagrampost/Yash/MainActivity.kt
@@ -22,13 +22,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun getMyDataop(): ArrayList<MyInstaModel> {
         var list=ArrayList<MyInstaModel>()
-        var myuser1=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
-        var myuser2=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
-        var myuser3=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
-        var myuser4=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
-        var myuser5=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
-        var myuser6=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
-        var myuser7=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser1=MyInstaModel(R.drawable.imag18,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser2=MyInstaModel(R.drawable.imag18,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser3=MyInstaModel(R.drawable.imag18,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser4=MyInstaModel(R.drawable.imag18,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser5=MyInstaModel(R.drawable.imag18,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser6=MyInstaModel(R.drawable.imag18,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser7=MyInstaModel(R.drawable.imag18,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
 
         list.add(myuser1)
         list.add(myuser2)
