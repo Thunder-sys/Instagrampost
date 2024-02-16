f72b07af1914a9e8ecf9485a140fc8c96d6a6fad
I58gen <yashpal14700@gmail.com>
2024-02-16 16:03:21 +0530

    updatepro 1

--- app/src/main/java/com/example/instagrampost/Yash/MainActivity.kt
+++ app/src/main/java/com/example/instagrampost/Yash/MainActivity.kt
@@ -23,8 +23,20 @@ class MainActivity : AppCompatActivity() {
     private fun getMyDataop(): ArrayList<MyInstaModel> {
         var list=ArrayList<MyInstaModel>()
         var myuser1=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser2=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser3=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser4=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser5=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser6=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
+        var myuser7=MyInstaModel(R.drawable.images2,R.drawable.images2,"My First User","Ham Vatan","230 likes","277 comments","120","2 days ago")
 
         list.add(myuser1)
+        list.add(myuser2)
+        list.add(myuser4)
+        list.add(myuser3)
+        list.add(myuser5)
+        list.add(myuser6)
+        list.add(myuser7)
         return list
 
     }
