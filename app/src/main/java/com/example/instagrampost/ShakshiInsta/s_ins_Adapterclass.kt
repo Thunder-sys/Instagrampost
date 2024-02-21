9f05fb336cc23fa0d484b5f2d36dee235ecd8d19
lappy <shakshik732@gmail.com>
2024-02-21 16:09:17 +0530

    i add changes

--- app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_Adapterclass.kt
+++ app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_Adapterclass.kt
@@ -23,7 +23,7 @@ class s_ins_Adapterclass(var context: Context,var sitem:ArrayList<s_ins_modelcla
     }
 
     override fun getItemId(position: Int): Long {
-       return position.toLong()
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -35,28 +35,30 @@ class s_ins_Adapterclass(var context: Context,var sitem:ArrayList<s_ins_modelcla
             var myinflaot =
                 context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             view = myinflaot.inflate(R.layout.s_i_itemview, null)
-            viewHolder = s_ins_Adapterclass.ViewHolder(view)
+            viewHolder = ViewHolder(view)
             view?.tag = viewHolder
         } else {
             view = convertView
             viewHolder = view.tag as ViewHolder
         }
 
-        var sitemm=sitem[position]
-       viewHolder.img?.setImageResource(sitemm.i_imag)
-       viewHolder.imag?.setImageResource(sitemm.i_mainimg)
-       viewHolder.save?.text=sitemm.i_save
-       viewHolder.share?.text=sitemm.i_share
-       viewHolder.comment?.text=sitemm.i_comment
-       viewHolder.like?.text=sitemm.i_like
+        var sitemm = sitem[position]
+        viewHolder.img?.setImageResource(sitemm.i_imag)
+        viewHolder.imag?.setImageResource(sitemm.i_mainimg)
+        viewHolder.comment?.text = sitemm.i_ncomm
+        viewHolder.like?.text = sitemm.i_nlik
+        viewHolder.wiew?.text = sitemm.i_nview
 
+        return view as View
     }
+
 
     private class ViewHolder(row:View){
         var img=row.findViewById<ImageView>(R.id.profile)
         var imag=row.findViewById<ImageView>(R.id.Image)
-        var save=row.findViewById<TextView>(R.id.save)
-        var save=row.findViewById<TextView>(R.id.save)
+        var comment=row.findViewById<TextView>(R.id.ncomment)
+        var like=row.findViewById<TextView>(R.id.ncomment)
+        var wiew=row.findViewById<TextView>(R.id.view)
     }
 
 
--- app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_modelclass.kt
+++ app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_modelclass.kt
@@ -8,6 +8,9 @@ class s_ins_modelclass {
     var i_like:String=""
     var i_user:String=""
     var i_mainimg:Int=0
+    var i_ncomm:String=""
+    var i_nlik:String=""
+    var i_nview:String=""
 
     //alt insert select all for constructor
     constructor(i_imag:Int,i_comment:String,i_share:Int,i_save:Int,i_like:String,i_user:String,i_mainimg:Int){
@@ -18,5 +21,8 @@ class s_ins_modelclass {
         this.i_like=i_like
         this.i_user=i_user
         this.i_mainimg=i_mainimg
+        this.i_ncomm=i_ncomm
+        this.i_nlik=i_nlik
+        this.i_nview=i_nview
     }
 }
