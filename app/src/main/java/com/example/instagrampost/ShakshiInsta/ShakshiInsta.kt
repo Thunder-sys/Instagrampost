44270066df99bcf118d469b9730f7490f52cae06
lappy <shakshik732@gmail.com>
2024-02-21 16:39:14 +0530

    i add changes

--- app/src/main/java/com/example/instagrampost/ShakshiInsta/ShakshiInsta.kt
+++ app/src/main/java/com/example/instagrampost/ShakshiInsta/ShakshiInsta.kt
@@ -2,12 +2,34 @@ package com.example.instagrampost.ShakshiInsta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import com.example.instagrampost.R
 
 class ShakshiInsta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shakshi_insta)
+
+        var inlist=findViewById<ListView>(R.id.mlist)
+
+        var inadapter=s_ins_Adapterclass(this,getMyData())
+
     }
-    
+
+    private fun getMyData(): ArrayList<s_ins_modelclass> {
+        var inslist=ArrayList<s_ins_modelclass>()
+        var i1=s_ins_modelclass(R.drawable.download1,"super",R.drawable.download1)
+        var i2=s_ins_modelclass(R.drawable.download1,"super",R.drawable.imag18)
+        var i3=s_ins_modelclass(R.drawable.download1,"super",R.drawable.images4)
+        var i4=s_ins_modelclass(R.drawable.download1,"super",R.drawable.images2)
+
+        inslist.add(i1)
+        inslist.add(i2)
+        inslist.add(i3)
+        inslist.add(i4)
+        return inslist
+
+    }
+
+
 }
--- app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_modelclass.kt
+++ app/src/main/java/com/example/instagrampost/ShakshiInsta/s_ins_modelclass.kt
@@ -3,23 +3,17 @@ package com.example.instagrampost.ShakshiInsta
 class s_ins_modelclass {
     var i_imag:Int=0
     var i_comment:String=""
-    var i_share:Int=0
-    var i_save:Int=0
-    var i_like:String=""
-    var i_user:String=""
+
     var i_mainimg:Int=0
     var i_ncomm:String=""
     var i_nlik:String=""
     var i_nview:String=""
 
     //alt insert select all for constructor
-    constructor(i_imag:Int,i_comment:String,i_share:Int,i_save:Int,i_like:String,i_user:String,i_mainimg:Int){
+    constructor(i_imag:Int,i_comment:String,i_mainimg:Int){
         this.i_imag=i_imag
         this.i_comment=i_comment
-        this.i_share=i_share
-        this.i_save=i_save
-        this.i_like=i_like
-        this.i_user=i_user
+
         this.i_mainimg=i_mainimg
         this.i_ncomm=i_ncomm
         this.i_nlik=i_nlik
